265225d971d6498f59374aa67b688fda2d949ed5
Rafael M. Martins <rafael.martins@lnu.se>
2020-04-07 12:59:45 +0200

    Removed cache dirs from repository

--- cachedir/joblib/tsneGrid/shepard_diagram_correlation/func_code.py
+++
@@ -1,8 +0,0 @@
-# first line: 185
-def shepard_diagram_correlation(D_high, D_low):
-    if len(D_high.shape) > 1:
-        D_high = spatial.distance.squareform(D_high)
-    if len(D_low.shape) > 1:
-        D_low = spatial.distance.squareform(D_low)
-
-    return stats.spearmanr(D_high, D_low)[0]
